bb56e64e1290436777dae9eedf88b280dd7553a4
Shige1962 <80299389+Shige1962@users.noreply.github.com>
2021-08-19 10:33:04 +0900

    Adding exit()

--- file_move.py
+++ file_move.py
@@ -1,6 +1,6 @@
 import os, shutil 
 
-#移動したいフォルダにすでに同じファイルがあると上書きされます
+# 注意：このプログラムは移動したいフォルダにすでに同じファイルがあると上書きされます
 
 print("移動したいファイルのあるパスを入力してください")
 print("例: /Users/username/Downloads/")
@@ -14,13 +14,14 @@ print("移動したいファイルの拡張子を入力してください")
 print("例: .csv (ドットの入力を忘れずに）")
 file_extension = input()
 
-if os.path.exists(root_path) == False:
+if os.path.exists(root_path) == False: #片方しかチェックしてない
     print("パスが存在しません")
+    exit()
 else: 
     os.chdir(root_path)
 
 for move_filename in os.listdir('.'):
     if not move_filename.endswith(file_extension):
-        continue # CSVファイルでなければスキップ ❶
+        continue # CSVファイルでなければスキップ 
     shutil.move(move_filename, new_path)
     print(move_filename + "は" + new_path + "に移動されました")
